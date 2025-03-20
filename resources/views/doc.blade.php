ab5182f946f0dfa6d49beeadbe9c0efc2281720a
vgabrielk <vfelix1553@gmail.com>
2025-03-20 14:32:27 -0300

    feat: Add external documentation to first application route.

---
+++ resources/views/doc.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentação API</title>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        iframe {
+            width: 100%;
+            height: 100vh;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+<iframe src="https://produtos-c.redocly.app/"></iframe>
+</body>
+</html>
